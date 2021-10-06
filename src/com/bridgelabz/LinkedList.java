2cabbba1ae311c23f0df9aea70f973973c62571f
koresachin7 <koresachin7@gmail.com>
2021-10-06 23:28:03 +0530

    Added : Add element to the linkedList

--- src/com/bridgelabz/LinkedList.java
+++ src/com/bridgelabz/LinkedList.java
@@ -1,21 +1,20 @@
 package com.bridgelabz;
 
 /**
- * Purpose : - To create a Linked List
- * @author Sachin Kore
+ * Purpose - To create a Linked List and add elements
+ * Final sequence to be printed - 56 - 30 - 70
+ * @author - Sachin kore
  * @version - 16.0
  */
-
 public class LinkedList {
     public static void main(String[] args) {
-        MyNode<Integer> myFirstNode = new MyNode<>(56);
+        MyNode<Integer> myFirstNode = new MyNode<>(70);
         MyNode<Integer> mySecondNode = new MyNode<>(30);
-        MyNode<Integer> myThirdNode = new MyNode<>(70);
-        myFirstNode.setNext(mySecondNode);
-        mySecondNode.setNext(myThirdNode);
-        boolean result = myFirstNode.getNext().equals(mySecondNode) && mySecondNode.getNext().equals(myThirdNode);
-        System.out.println(result);
-
+        MyNode<Integer> myThirdNode = new MyNode<>(56);
+        MyLinkedList myLinkedList = new MyLinkedList();
+        myLinkedList.add(myFirstNode);
+        myLinkedList.add(mySecondNode);
+        myLinkedList.add(myThirdNode);
+        myLinkedList.displayNode();
     }
-
 }
